bfcded0bb20333f7324557d337deccdf3d4daa4b
giangkhuat <khuatgia@grinnell.edu>
2019-07-20 10:00:26 -0500

    Prepare to deploy

--- app/config.py
+++ app/config.py
@@ -7,7 +7,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
 	#SECRET_KEY = os.environ.get('SECRET_KEY')
-	SECRET_KEY=getenv('SECRET_KEY', None)
+	SECRET_KEY = getenv('SECRET_KEY', None)
 	assert SECRET_KEY
 	#SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
 	SQLALCHEMY_DATABASE_URI=getenv('SQLALCHEMY_DATABASE_URI', None)
